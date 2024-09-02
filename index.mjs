1ff0fb04ee387dab252ea539943226fb6b50811e
Figby222 <figby222@outlook.com>
2024-09-01 21:44:51 -0700

    Setup app to look for views in views/ directory

--- index.mjs
+++ index.mjs
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
 const app = express();
+import path from "node:path";
 import { getRoot } from "./controllers/rootController.mjs";
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", getRoot);
 
