50a670bd6b5e50ef723a8db31e90004f67063901
Figby222 <figby222@outlook.com>
2024-09-02 04:19:48 -0700

    Add links & pass as prop to getRootMessage render

--- controllers/rootController.mjs
+++ controllers/rootController.mjs
@@ -2,8 +2,13 @@ function getRoot(req, res) {
     return res.send("Hi");
 }
 
+const links = [
+    { href: "/", text: "Home" },
+    { href: "about", text: "About" },
+];
+
 function getRootMessage(req, res) {
-    return res.render("index", { message: "EJS IS AWESOME" });
+    return res.render("index", { message: "EJS IS AWESOME", links: links });
 }
 
 export { getRoot, getRootMessage };
