538c6a428d94a2af3979aea411ebf75b06280f26
Figby222 <figby222@outlook.com>
2024-09-01 21:42:42 -0700

    Setup root Route

--- index.mjs
+++ index.mjs
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
 const app = express();
+import { getRoot } from "./controllers/rootController.mjs";
+
+app.get("/", getRoot);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
