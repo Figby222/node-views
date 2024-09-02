9154fe102659b863a4d755b483dc99ef4e6da005
Figby222 <figby222@outlook.com>
2024-09-01 22:17:20 -0700

    Use path.resolve & res.render to render animals
    Use path.resolve & res.render to render animals
    view

--- controllers/animalsController.mjs
+++ controllers/animalsController.mjs
@@ -1,5 +1,7 @@
+import path from "node:path";
+
 function getAnimals (req, res) {
-    return res.sendFile("../views/animalsRoot.ejs");
+    return res.render(path.resolve("views/animals.ejs"));
 }
 
 export { getAnimals };
