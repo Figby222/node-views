67dc1bb22812ff8292a2a00ffed768f44293457b
Figby222 <figby222@outlook.com>
2024-09-01 22:03:38 -0700

    Init __dirname

--- index.mjs
+++ index.mjs
@@ -2,10 +2,15 @@ import express from "express";
 import "dotenv/config";
 const app = express();
 import path from "node:path";
+import animalsRouter from "./routers/animalsRouter.mjs";
 import { getRoot } from "./controllers/rootController.mjs";
+
+const __dirname = import.meta.dirname;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/animals", animalsRouter);
 
 app.get("/", getRoot);
 
