92c9a9ba0de739cd44879829acac9365251eee09
Figby222 <figby222@outlook.com>
2024-09-02 04:07:16 -0700

    Render index view on /message

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import "dotenv/config";
 const app = express();
 import path from "node:path";
 import animalsRouter from "./routers/animalsRouter.mjs";
-import { getRoot } from "./controllers/rootController.mjs";
+import { getRoot, getRootMessage } from "./controllers/rootController.mjs";
 
 const __dirname = import.meta.dirname;
 
@@ -13,6 +13,8 @@ app.set("view engine", "ejs");
 app.use("/animals", animalsRouter);
 
 app.get("/", getRoot);
+
+app.get("/message", getRootMessage);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}`)
