215805bfc0a4d1a8821d22beca3d1bc0f79264f2
Figby222 <figby222@outlook.com>
2024-09-02 05:25:49 -0700

    Pass secretFooterMessage prop to each rendered page

--- controllers/rootController.mjs
+++ controllers/rootController.mjs
@@ -10,11 +10,20 @@ const links = [
 const users = [ "Rose", "Cake", "Cookies" ];
 
 function getRootMessage(req, res) {
-    return res.render("index", { message: "EJS IS AWESOME", links: links, users: users });
+    return res.render("index", { 
+        message: "EJS IS AWESOME", 
+        links: links, 
+        users: users,
+        secretFooterMessage: "Hi, I'm the secret footer message"
+    });
 }
 
 function getAboutPage(req, res) {
-    return res.render("about", { secretMessage: "Hi, I'm a secret message", links: links });
+    return res.render("about", { 
+        secretMessage: "Hi, I'm a secret message", 
+        links: links,
+        secretFooterMessage: "Hi, I'm the secret footer message"
+     });
 }
 
 export { getRoot, getRootMessage, getAboutPage };
